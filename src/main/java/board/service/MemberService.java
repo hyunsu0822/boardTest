d761667fe8a4734a825e7ff7908746e01d5c82a6
hspark <hyunsu0822@gmail.com>
2018-05-17 13:35:32 +0900

    MemberDao 오류 수정

--- src/main/java/board/service/MemberService.java
+++ src/main/java/board/service/MemberService.java
@@ -27,58 +27,7 @@ public class MemberService implements MemberServiceInf {
 		return memberDao.loginProcess(memberVO);
 	}
 
-	@Override
-	public Map<String, Object> getMemberPageList(int page, int pageSize) {
-		Map<String, Object> resultMap = new HashMap<String, Object>();
-		
-		List<MemberVO> memberList = memberDao.getMemberPageList(page, pageSize);
-		String pageNav = makePageNav(page, memberDao.getMemberTotalCnt());
-		
-		resultMap.put("memberList", memberList);
-		resultMap.put("pageNav", pageNav);
-		
-		return resultMap;
-	}
-
-	private String makePageNav(int page, int memberTotalCnt) {
-		
-		int pageTotalCnt = (int)Math.ceil( (double)memberTotalCnt / 10);
-		
-		StringBuffer pageNav = new StringBuffer();
-		pageNav.append("<nav>");
-		pageNav.append("	<ul class=\"pagination\">");
-		pageNav.append("		<li>");
-		pageNav.append("			<a href=\"getMemberPageList?page=1&pageSize=10\" aria-label=\"Previous\">");
-		pageNav.append("				<span aria-hidden=\"true\">&laquo;</span>");
-		pageNav.append("			</a>");
-		pageNav.append("		</li>");
-
-		
-		for(int i = 1; i <= pageTotalCnt; i++){
-			if( i == page)
-				pageNav.append("		<li class=\"active\"><a href=\"getMemberPageList?page=" + i + "&pageSize=10\">" + i + "</a></li>");
-			else
-				pageNav.append("		<li ><a href=\"getMemberPageList?page=" + i + "&pageSize=10\">" + i + "</a></li>");
-		}
-		
-		pageNav.append("		<li>");
-		pageNav.append("			<a href=\"getMemberPageList?page=" + pageTotalCnt + "&pageSize=10\" aria-label=\"Next\">");
-		pageNav.append("				<span aria-hidden=\"true\">&raquo;</span>");
-		pageNav.append("			</a>");
-		pageNav.append("		</li>");
-		pageNav.append("	</ul>");
-		pageNav.append("</nav>");
-		    
-		return pageNav.toString();
-
-		
-	}
-
-	@Override
-	public MemberVO getMember(String mem_id) {
-		
-		return memberDao.getMember(mem_id);
-	}
+	
 
 	
 
--- src/main/java/board/service/MemberServiceInf.java
+++ src/main/java/board/service/MemberServiceInf.java
@@ -17,32 +17,6 @@ public interface MemberServiceInf {
 	 */
 	public MemberVO loginProcess(MemberVO memberVO);
 	
-	/** 
-	 * Method   : getMemberPageList
-	 * 최초작성일  : 2018. 5. 14. 
-	 * 작성자 : "P.H.S"
-	 * 변경이력 : 
-	 * @param page
-	 * @param pageSize
-	 * @return 
-	 * Method 설명 : 회원 페이지 list 조회
-	 */
-	
-	public Map<String, Object> getMemberPageList(int page, int pageSize);
-
-	/** 
-	 * Method   : getMember
-	 * 최초작성일  : 2018. 5. 14. 
-	 * 작성자 : "P.H.S"
-	 * 변경이력 : 
-	 * @param mem_id
-	 * @return 
-	 * Method 설명 : 회원 상세정보 조회 
-	 */
-	
-	public MemberVO getMember(String mem_id);
-	
-	
 	
 }
 
