8bbda2f9c023988054c7bc0eccc030c2f59d6a58
hspark <hyunsu0822@gmail.com>
2018-05-17 14:14:55 +0900

    로그인 수정

--- src/test/java/board/dao/MemberDaoTest.java
+++ src/test/java/board/dao/MemberDaoTest.java
@@ -10,78 +10,11 @@ import board.model.MemberVO;
 
 public class MemberDaoTest {
 
-	/**
-	 * 
-	* Method : getMemberPageListTest
-	* 최초작성일 : 2018. 5. 15.
-	* 작성자 : "P.H.S"
-	* 변경이력 :
-	* Method 설명 : 회원 페이지 list 조회 테스트
-	 */
-	@Test
-	public void getMemberPageListTest() {
-		
-		/***Given***/
-		MemberDaoInf memberDao = new MemberDao();
-		int page = 1;
-		int pageSize = 10;
-		
-		/***When***/
-		List<MemberVO> memberList = memberDao.getMemberPageList(page, pageSize);
-		for(MemberVO member : memberList)
-			System.out.println(member);
-		
-		/***Then***/
-		assertNotNull(memberList);
-		assertEquals(10, memberList.size());
-		
-	}
 	
 	
-	/**
-	 * 
-	* Method : getMemberTotalCntTest
-	* 최초작성일 : 2018. 5. 15.
-	* 작성자 : "P.H.S"
-	* 변경이력 :
-	* Method 설명 : 사용자 전체 건수조회
-	 */
-	@Test
-	public void getMemberTotalCntTest(){
-		
-		/***Given***/
-		MemberDaoInf memberDao = new MemberDao();
-		
-		/***When***/
-		int memberTotalCnt = memberDao.getMemberTotalCnt();
+	
 
-		/***Then***/
-		assertEquals(23, memberTotalCnt);
-	}
-
-	/**
-	 * 
-	* Method : getMemberTest
-	* 최초작성일 : 2018. 5. 15.
-	* 작성자 : "P.H.S"
-	* 변경이력 :
-	* Method 설명 : 회원 상세정보 조회 테스트
-	 */
-	@Test
-	public void getMemberTest(){
-		/***Given***/
-		MemberDaoInf memberDao = new MemberDao();
-		String mem_id = "a001";
-		
-		/***When***/
-		MemberVO memberVO = memberDao.getMember(mem_id);
-		
-		/***Then***/
-		assertEquals(mem_id, memberVO.getMem_id());
-		assertEquals("1111", memberVO.getMem_pass());
-		assertEquals("김은대", memberVO.getMem_name());	
-	}
-
+	
 	
 	
 }
--- src/test/java/board/service/MemberServiceTest.java
+++ src/test/java/board/service/MemberServiceTest.java
@@ -13,57 +13,6 @@ import board.model.MemberVO;
 
 public class MemberServiceTest {
 
-	/**
-	 * 
-	* Method : getMemberPageListTest
-	* 최초작성일 : 2018. 5. 15.
-	* 작성자 : "P.H.S"
-	* 변경이력 :
-	* Method 설명 : 회원 페이지 list 조회 테스트
-	 */
-	@Test
-	public void getMemberPageListTest() {
-		
-		/***Given***/
-		MemberServiceInf memberService = new MemberService();
-		int page = 1;
-		int pageSize = 10;
-		
-		/***When***/
-		Map<String, Object> resultMap = memberService.getMemberPageList(page, pageSize);
-		
-		/***Then***/
-		List<MemberVO> memberList = (List<MemberVO>)resultMap.get("memberList");
-		
-		assertNotNull(memberList);
-		assertEquals(10, memberList.size());
-		
-	}
 	
-	
-
-	/**
-	 * 
-	* Method : getMemberTest
-	* 최초작성일 : 2018. 5. 15.
-	* 작성자 : "P.H.S"
-	* 변경이력 :
-	* Method 설명 : 회원 상세정보 조회 테스트
-	 */
-	@Test
-	public void getMemberTest(){
-		/***Given***/
-		MemberServiceInf memberService = new MemberService();
-		String mem_id = "a001";
-		
-		/***When***/
-		MemberVO memberVO = memberService.getMember(mem_id);
-		
-		/***Then***/
-		assertEquals(mem_id, memberVO.getMem_id());
-		assertEquals("1111", memberVO.getMem_pass());
-		assertEquals("김은대", memberVO.getMem_name());	
-	}
-
 
 }
